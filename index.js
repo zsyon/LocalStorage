85f6a11422a28263df1a475f446a056b66d1e55f
HP <20060339@stu.omu.edu.tr>
2023-02-22 22:15:32 +0300

    edit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 let arttır = document.addEventListener(`click`, function(){
+  
   caunter = document.getElementById("sayı").innerHTML
-  console.log(caunter)
   sayi = Number(caunter) + 1
   document.getElementById("sayı").innerHTML = caunter
-  caunter = localStorage.getItem('caunter');
-  let arttir = localStorage.setItem('caunter', caunter)
-  localStorage.getItem('caunter') ? localStorage.getItem('caunter') : 0
+  
+
 })
